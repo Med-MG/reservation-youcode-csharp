d5a0596ab949699d82fc4db6d3198c1f071d9ca8
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-03-13 17:14:29 +0100

    adding mediatR, refactoring our business logic

--- API/Controllers/ReservationsController.cs
+++ API/Controllers/ReservationsController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.reservations;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -10,23 +12,26 @@ namespace API.Controllers
 {
     public class ReservationsController : BaseApiController
     {
-               private readonly DataContext _context;
 
-        public ReservationsController(DataContext context)
+        private readonly IMediator _mediator;
+
+        public ReservationsController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
+
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Reservation>>> GetReservations()
         {
-            return await _context.Reservations.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] // Reservation/id
-        public async Task<ActionResult<Reservation>> GetReservation(Guid id){
-            return await _context.Reservations.FindAsync(id);
+        public async Task<ActionResult<Reservation>> GetReservation(Guid id)
+        {
+            return Ok();
         }
-        
+
     }
 }
