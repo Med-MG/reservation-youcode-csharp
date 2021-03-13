86aad2e2af3d15d2fc7746b8c74ab7cfb612ffa8
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-03-13 18:10:21 +0100

    Adding autoMapper

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Core;
 using Application.reservations;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
@@ -47,6 +48,7 @@ namespace API
             });
 
             services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
         }
 
--- Application/reservations/Edit.cs
+++ Application/reservations/Edit.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -8,15 +9,18 @@ namespace Application.reservations
 {
     public class Edit
     {
-        public class Command : IRequest{
+        public class Command : IRequest
+        {
             public Reservation Reservation { get; set; }
         }
 
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
@@ -24,7 +28,9 @@ namespace Application.reservations
             {
                 var Reservation = await _context.Reservations.FindAsync(request.Reservation.Id);
 
-                Reservation.Title = request.Reservation.Title ?? Reservation.Title;
+                // Reservation.Title = request.Reservation.Title ?? Reservation.Title;
+
+                _mapper.Map(request.Reservation, Reservation);
 
                 await _context.SaveChangesAsync();
 
