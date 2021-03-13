d24330553598922adbef3a77686d3b010c1f54f2
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-03-13 14:15:12 +0100

    Cors Origin policy startup cs

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -4,13 +4,12 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [reservations, setReservations] = useState([])
+  const [Reservations, setReservations] = useState([])
 
   useEffect(() => { 
     axios.get("http://localhost:5000/api/Reservations").then(response => {
       console.log(response.data)
       setReservations(response.data)
-      
     })
   }, [])
 
@@ -19,9 +18,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <ul>
-          {reservations.map((reservation: any) => {
+          {Reservations.map((reservation: any) => 
             <li key={reservation.id} >{reservation.title}</li>
-          })}
+          )}
         </ul>
       </header>
     </div>
